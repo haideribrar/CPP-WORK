1c9c8a28bdce956a734817e6f37712851babc2f3
haideribrar <haideribrar193@gmail.com>
2025-11-08 00:25:46 +0500

    Add files via upload

---
+++ DO-WHILE LOOPS, LOOPS/Q1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cout << "Enter any Positive Integer: ";
+	cin >> a;
+	if (a <= 0)
+	{
+		cout << "Invalid Input"<<endl;
+		return 0;
+	}
+	int b, i = 0;
+	int temp = a, counter = 0;
+	while (i<10)
+	{
+		while (temp > 0)
+		{
+			b = temp % 10;
+			temp = temp / 10;
+			if (b == i)
+			{
+				counter++;
+			}
+		}
+		cout << "Digit " << i << " => " << counter << " times" << endl;
+		counter = 0;
+		temp = a;
+		i++;
+	}
+}
---
+++ DO-WHILE LOOPS, LOOPS/Q9.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a;
+	cout << "Enter any Positive Integer: ";
+	cin >> a;
+	if (a <= 0)
+	{
+		cout << "Invalid Input";
+		return 0;
+	}
+	int i = 1,sum=0;
+	while (i<a)
+	{
+		if (a % i == 0)
+		{
+			sum = sum + i;
+		}
+		i++;
+	}
+	if (sum == a)
+	{
+		cout << a << " is a Perfect Number."<<endl;
+	}
+	else
+	{
+		cout << a << " is Not a Perfect Number." << endl;
+	}
+	return 0;
+}
