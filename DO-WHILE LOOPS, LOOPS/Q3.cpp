3db7ce15a8b8bb8925c79eeb8bab7736adbf44b0
haideribrar <haideribrar193@gmail.com>
2025-11-08 00:22:33 +0500

    Add files via upload

---
+++ DO-WHILE LOOPS, LOOPS/Q3.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    int num = -1, binnum = 0, reverse = 0, digitcount = 0;
+//
+//    while (num < 0) {
+//        cout << "enter a non-negative number: ";
+//        cin >> num;
+//    }
+//
+//    while (num > 0) {
+//        binnum = (binnum * 10) + (num % 2);
+//        num /= 2;
+//        digitcount++;
+//    }
+//
+//    while (digitcount > 0) {
+//        reverse = (reverse * 10) + (binnum % 10);
+//        binnum /= 10;
+//        digitcount--;
+//    }
+//
+//    cout << "binary number: " << reverse << endl;
+//
+//    return 0;
+//}
---
+++ DO-WHILE LOOPS, LOOPS/Q7.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	int a;
+//	cout << "Enter the size of the Pyramid: ";
+//	cin >> a;
+//	for (int i = 1; i <= a; i++)
+//	{
+//		for (int j =i; j<=a-1; j++)
+//		{
+//			cout << " ";
+//		}
+//		for (int k = 1; k <= 2*i-1; k++)
+//		{
+//			if (k ==1||k==2*i-1||i==a )
+//			{
+//				cout << "*";
+//			}
+//			else
+//			{
+//				cout << " ";
+//			}
+//		}
+//		cout << endl;
+//	}
+//}
